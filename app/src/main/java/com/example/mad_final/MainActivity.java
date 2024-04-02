b473f80d77ad287d4928ef235bc53d9f4c998fe2
Vivian Morton <vivian.morton404@gmail.com>
2024-04-02 12:56:24 -0400

    Added simple menu, thought it doesn't inflate

--- app/src/main/java/com/example/mad_final/MainActivity.java
+++ app/src/main/java/com/example/mad_final/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.mad_final;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
