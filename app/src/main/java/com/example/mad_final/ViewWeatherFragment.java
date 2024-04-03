b5c24760045d0140685b41ef91fb83185961d9b9
Vivian Morton <vivian.morton404@gmail.com>
2024-04-03 10:31:20 -0400

    Now gets location

--- app/src/main/java/com/example/mad_final/ViewWeatherFragment.java
+++ app/src/main/java/com/example/mad_final/ViewWeatherFragment.java
@@ -8,19 +8,32 @@
 
 package com.example.mad_final;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class ViewWeatherFragment extends Fragment {
 
+
     private Handler handler = new Handler();
+    private FusedLocationProviderClient fusedLocationClient;
 
 
     public ViewWeatherFragment() {
@@ -44,7 +57,30 @@ public class ViewWeatherFragment extends Fragment {
 
         APIHandler apiHandler = new APIHandler(handler, getContext());
         apiHandler.getWeather();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
 
+        if (ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 0);
+            ActivityCompat.requestPermissions(requireActivity(), new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
+
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+
+                })
+                .addOnFailureListener(requireActivity(), new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ViewWeatherFragment", "Failed to get location");
+                    }
+                });
 
 
         return inflater.inflate(R.layout.fragment_view_weather, container, false);
