ab9677c1687becf746bbd42d39c83cfe519ea709
IribeiroLeao2003 <iribeiroleao9600@conestogac.on.ca>
2024-04-09 11:33:15 -0400

    Fixed Values not showing

--- app/src/main/java/com/example/mad_final/ViewWeatherFragment.java
+++ app/src/main/java/com/example/mad_final/ViewWeatherFragment.java
@@ -9,6 +9,7 @@
 package com.example.mad_final;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -22,6 +23,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -29,10 +31,14 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.w3c.dom.Text;
+
 
 public class ViewWeatherFragment extends Fragment {
 
+    private TextView userLatitude;
 
+    private TextView userLongitude;
     private Handler handler = new Handler();
     private FusedLocationProviderClient fusedLocationClient;
 
@@ -54,22 +60,40 @@ public class ViewWeatherFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        View rootView = inflater.inflate(R.layout.fragment_view_weather, container, false);
+
+        // Initialize your TextViews here so they are ready for updates
+        userLatitude = rootView.findViewById(R.id.lat_loc);
+        userLongitude = rootView.findViewById(R.id.lon_loc);
+
+
         // Inflate the layout for this fragment
         if (ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(requireActivity(), new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 0);
             ActivityCompat.requestPermissions(requireActivity(), new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
 
         }
+
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
         fusedLocationClient.getLastLocation()
+
+
                 .addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onSuccess(Location location) {
+                        String longitude = Double.toString(location.getLongitude());
+                        String latitude = Double.toString(location.getLatitude());
+                        userLatitude.setText("Lat: " + longitude);
+                        userLongitude.setText("Lon: " + latitude);
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
                             Toast.makeText(requireContext(), "Latitude: " + String.valueOf(location.getLatitude()), Toast.LENGTH_SHORT).show();
                             Toast.makeText(requireContext(), "Longitude: " +String.valueOf(location.getLongitude()), Toast.LENGTH_SHORT).show();
-
                             APIHandler apiHandler = new APIHandler(handler, getContext(), location);
 
                         }
@@ -84,6 +108,6 @@ public class ViewWeatherFragment extends Fragment {
                     }
                 });
 
-        return inflater.inflate(R.layout.fragment_view_weather, container, false);
+        return rootView;
     }
 }
