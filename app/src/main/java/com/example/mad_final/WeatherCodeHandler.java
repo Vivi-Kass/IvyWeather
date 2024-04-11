390f88fbf7c482b0c0f698ddab515ab58b661878
Vivian Morton <vivian.morton404@gmail.com>
2024-04-10 20:08:58 -0400

    Added function to return a string of the icon to call based on the weather and time of day

--- app/src/main/java/com/example/mad_final/WeatherCodeHandler.java
+++ app/src/main/java/com/example/mad_final/WeatherCodeHandler.java
@@ -8,10 +8,26 @@
 
 package com.example.mad_final;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class WeatherCodeHandler {
+
+    private static int isday = 1;
+    private static List<Integer> sun = new ArrayList<>(Arrays.asList(0, 1));
+    private static List<Integer> sunCloud = new ArrayList<>(Arrays.asList(2));
+    private static List<Integer> moon = new ArrayList<>(Arrays.asList(0, 1));
+    private static List<Integer> moonCloud = new ArrayList<>(Arrays.asList(2));
+    private static List<Integer> cloud = new ArrayList<>(Arrays.asList(3, 45, 48));
+    private static List<Integer> rain = new ArrayList<>(Arrays.asList(51, 53, 55, 61, 63, 65, 80, 81, 82));
+    private static List<Integer> freezingRain = new ArrayList<>(Arrays.asList(56, 57, 66, 67));
+    private static List<Integer> snow = new ArrayList<>(Arrays.asList(71, 73, 75, 77, 85, 86));
+    private static List<Integer> thunder = new ArrayList<>(Arrays.asList(95));
+
     private static Map<Integer, String> weatherCodes = new HashMap<Integer, String>() {{
         //Sky
         put(0, "Clear Sky");
@@ -59,6 +75,62 @@ public class WeatherCodeHandler {
     public static String weatherStatus(int code)
     {
         return weatherCodes.get(code);
+    }
+
+    public static String getIcon(int weatherCode, int dayStatus)
+    {
+        String iconName = "";
+        if (weatherCode >= 3) //no moon or sun
+        {
+            if (cloud.contains(weatherCode))
+            {
+                iconName = "Cloud";
+            }
+            else if (rain.contains(weatherCode))
+            {
+                iconName = "Rain";
+            }
+            else if (freezingRain.contains(weatherCode))
+            {
+                iconName = "FreezingRain";
+            }
+            else if (snow.contains(weatherCode))
+            {
+                iconName = "Snow";
+            }
+            else if (thunder.contains(weatherCode))
+            {
+                iconName = "Thunder";
+            }
+        }
+        else //moon or sun
+        {
+            if (dayStatus == isday) //sun icon
+            {
+                if (sun.contains(weatherCode))
+                {
+                    iconName = "Sun";
+                }
+                else if (sunCloud.contains(weatherCode))
+                {
+                    iconName = "SunCloud";
+                }
+
+            }
+            else //moon icon
+            {
+                if (moon.contains(weatherCode))
+                {
+                    iconName = "Moon";
+                }
+                else if (moonCloud.contains(weatherCode))
+                {
+                    iconName = "MoonCloud";
+                }
+            }
+        }
+
+        return iconName;
     }
 
 
