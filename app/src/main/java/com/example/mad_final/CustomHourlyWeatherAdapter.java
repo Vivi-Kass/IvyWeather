50154f8ab78d412348484e5f1e9c1b832b506abb
Vivian Morton <vivian.morton404@gmail.com>
2024-04-11 17:04:36 -0400

    Hourly weather list view now populated with time and date

--- app/src/main/java/com/example/mad_final/CustomHourlyWeatherAdapter.java
+++ app/src/main/java/com/example/mad_final/CustomHourlyWeatherAdapter.java
@@ -1,6 +1,9 @@
 package com.example.mad_final;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +11,16 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class CustomHourlyWeatherAdapter extends BaseAdapter {
     private Context context;
     private static int hoursCount = 168; //7 days each with 24 hours
+    private int offset;
     private LayoutInflater inflater;
+    private final int dateloc = 0;
+    private final int timeloc = 1;
     public CustomHourlyWeatherAdapter(Context ctx)
     {
         context = ctx;
@@ -21,7 +30,10 @@ public class CustomHourlyWeatherAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return hoursCount;
+        Date currentTime = Calendar.getInstance().getTime();
+        offset = currentTime.getHours();
+        int total = hoursCount - offset; //subtract the passed time from the total number
+        return total;
     }
 
     @Override
@@ -39,6 +51,10 @@ public class CustomHourlyWeatherAdapter extends BaseAdapter {
 
         convertView = inflater.inflate(R.layout.activity_hourly_weather_list_view, null);
 
+        int realPosition = position + offset;
+        String dayTimeString[] = null;
+
+        TextView date = convertView.findViewById(R.id.hourly_date);
         TextView time = convertView.findViewById(R.id.hourly_time);
         ImageView icon = convertView.findViewById(R.id.weather_icon_hourly);
         TextView code = convertView.findViewById(R.id.hourly_code);
@@ -47,9 +63,15 @@ public class CustomHourlyWeatherAdapter extends BaseAdapter {
         TextView precipitationProb = convertView.findViewById(R.id.hourly_precipitation_prob);
         TextView precipitationAmount = convertView.findViewById(R.id.hourly_precipitation_amount);
 
-        time.setText("Time:");
-
-
+        try{
+            dayTimeString = IvyWeather.getWeatherData().getJSONObject("hourly").getJSONArray("time").getString(realPosition).split("T");
+            date.setText(dayTimeString[dateloc]);
+            time.setText(dayTimeString[timeloc]);
+        }
+        catch (Exception e)
+        {
+            Log.d(TAG,"Failed to get JSON data for hourly weather");
+        }
 
         return convertView;
     }
