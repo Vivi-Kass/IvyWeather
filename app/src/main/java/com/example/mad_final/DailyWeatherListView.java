2b21bbe4c7fadc0f9797f2cc683fb5ff79fba186
Vivian Morton <vivian.morton404@gmail.com>
2024-04-13 15:09:35 -0400

    Began implementing daily weather

---
+++ app/src/main/java/com/example/mad_final/DailyWeatherListView.java
@@ -0,0 +1,27 @@
+package com.example.mad_final;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mad_final.databinding.ActivityDailyWeatherListViewBinding;
+
+public class DailyWeatherListView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
