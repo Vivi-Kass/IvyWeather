f95fe5c24e037e2f441667e1e8b36fd7330fbf30
IribeiroLeao2003 <iribeiroleao9600@conestogac.on.ca>
2024-04-16 11:56:59 -0400

    Cleaned and commented credits page

--- app/src/main/java/com/example/mad_final/CreditsFragment.java
+++ app/src/main/java/com/example/mad_final/CreditsFragment.java
@@ -1,3 +1,12 @@
+/*
+ * FILE :            CreditsFragment.java
+ * PROJECT :         PROG3150 - Project
+ * PROGRAMMER :      Vivian Morton and Isaac Ribeiro Leao
+ * FIRST VERSION :   2024 - 04 - 02
+ * DESCRIPTION :     Contains the logic for the Credits Page
+ * CREDITS: https://www.vecteezy.com/free-vector/weather-icons Weather Icons Vectors by Vecteezy
+ */
+
 package com.example.mad_final;
 
 import android.content.Intent;
@@ -18,6 +27,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CreditsFragment extends Fragment {
+
+    //Variables created
     private final String isaacLink = "https://github.com/IribeiroLeao2003";
     private final String vivianLink = "https://github.com/vivi-kiwi";
     private final String iconCreditLink = "https://www.vecteezy.com/free-vector/weather-icons";
@@ -42,7 +53,7 @@ public class CreditsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
+        // set rootview and other textviews/ImageViews
         View rootView = inflater.inflate(R.layout.fragment_credits, container, false);
 
         TextView tvIsaacTitle = rootView.findViewById(R.id.tvIsaacTitle);
@@ -56,6 +67,9 @@ public class CreditsFragment extends Fragment {
 
         TextView apiCredit = rootView.findViewById(R.id.api_credits_text);
 
+
+
+        // set both icons and textviews as clickable, once clicked send to the respective github page
         tvIsaacTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,43 +86,43 @@ public class CreditsFragment extends Fragment {
 
         tvVivianTitle.setOnClickListener(new  View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                openLink(v,vivianLink);
-            }
+            public void onClick(View v) { openLink(v,vivianLink);}
         });
 
         vivianIcon.setOnClickListener(new  View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                openLink(v,vivianLink);
-            }
+            public void onClick(View v) { openLink(v,vivianLink);}
+
         });
 
+
+
+
+        // set the app logo clickable so once clicked, user is sent to the project's repository
         appLogo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openLink(v,repoLink);
-            }
+            public void onClick(View v){ openLink(v,repoLink); }
         });
 
 
+
+        //make the text views clickable, once clicked send user to the page credit of the owner of the API and Images used
         iconCredit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openLink(v,iconCreditLink);
-            }
+            public void onClick(View v) { openLink(v,iconCreditLink); }
         });
-
         apiCredit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openLink(v,APILink);
-            }
+            public void onClick(View v) { openLink(v, APILink); }
         });
 
-        // Inflate the layout for this fragment
+        // Return rootView
         return rootView;
     }
+//  METHOD NAME:   public void openLink(View view, String link):
+//  DESCRIPTION: This method grabs the string link and starts a new activity by sending the user to the URL
+//  PARAMETERS:  View view, String link
+//  RETURNS:     Nothing
 
     public void openLink(View view, String link) {
 
